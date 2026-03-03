39044fe13df08dd2eb8f5eb3c975ac765bfacf51
Кирилл Чернов <89049125360@mail.ru>
2026-03-03 19:28:51 +0300

    Добавлен менеджер записей целей

---
+++ GoalsTrackerApp/Managers/DataManager+Goal.swift
@@ -0,0 +1,73 @@
+//
+//  DataManager.swift
+//  GoalsTrackerApp
+//
+//  Created by Кирилл on 28.02.2026.
+//
+
+import Foundation
+import CoreData
+
+// MARK: - Protocols
+protocol GoalDataManagerProtocol {
+    
+    func createGoal(title: String, icon: String, color: CustomColor)
+    func readGoal(id: UUID) -> Goal?
+    
+}
+
+// MARK: - GoalDataManagerProtocol
+extension DataManager: GoalDataManagerProtocol {
+    
+    func createGoal(title: String, icon: String, color: CustomColor) {
+        context.performAndWait {
+            let request: NSFetchRequest<Goal> = Goal.fetchRequest()
+            
+            do {
+                let goals = try context.fetch(request)
+                for (index, goal) in goals.enumerated() {
+                    goal.position = Int16(index + 1)
+                }
+                
+                let goal = Goal(context: context)
+                goal.id = UUID()
+                goal.title = title
+                goal.icon = icon
+                goal.color = color.rawValue
+                goal.position = 0
+                goal.addedOn = Date()
+                goal.modifiedOn = Date()
+                
+                try context.save()
+            } catch {
+                fatalError("error: \(error)")
+            }
+        }
+    }
+    
+    func readGoal(id: UUID) -> Goal? {
+        var goal: Goal?
+        context.performAndWait {
+            let request: NSFetchRequest<Goal> = Goal.fetchRequest()
+            var predicates: [NSPredicate] = []
+            
+            if let predicate = request.predicate {
+                predicates.append(predicate)
+            }
+            
+            predicates.append(NSPredicate(format: "id = %@", argumentArray: [id]))
+            request.predicate = NSCompoundPredicate(
+                andPredicateWithSubpredicates: predicates
+            )
+            request.fetchLimit = 1
+            
+            do {
+                goal = try context.fetch(request).first
+            } catch {
+                fatalError("error: \(error)")
+            }
+        }
+        return goal
+    }
+    
+}
---
+++ GoalsTrackerApp/Managers/DataManager+GoalRecord.swift
@@ -0,0 +1,103 @@
+//
+//  DataManager+GoalRecord.swift
+//  GoalsTrackerApp
+//
+//  Created by Кирилл on 03.03.2026.
+//
+
+import Foundation
+import CoreData
+
+// MARK: - Protocols
+protocol GoalRecordDataManagerProtocol {
+    
+    func createGoalRecord(for goalID: UUID, date: Date)
+    func readGoalRecord(for goalID: UUID, date: Date) -> GoalRecord?
+    func deleteGoalRecord(for goalID: UUID, date: Date)
+    
+}
+
+extension GoalRecordDataManagerProtocol {
+    
+    func createGoalRecord(for goalID: UUID, date: Date = Date()) {
+        createGoalRecord(for: goalID, date: date)
+    }
+    func readGoalRecord(for goalID: UUID, date: Date = Date()) -> GoalRecord? {
+        readGoalRecord(for: goalID, date: date)
+    }
+    func deleteGoalRecord(for goalID: UUID, date: Date = Date()) {
+        deleteGoalRecord(for: goalID, date: date)
+    }
+    
+}
+
+// MARK: - GoalRecordDataManagerProtocol
+extension DataManager: GoalRecordDataManagerProtocol {
+    
+    func createGoalRecord(for goalID: UUID, date: Date) {
+        context.performAndWait {
+            guard let goal = readGoal(id: goalID) else { return }
+            let goalRecord = GoalRecord(context: context)
+            goalRecord.goal = goal
+            goalRecord.date = date
+            do {
+                try context.save()
+            } catch {
+                fatalError("error: \(error)")
+            }
+            
+        }
+    }
+    
+    func readGoalRecord(for goalID: UUID, date: Date) -> GoalRecord? {
+        var goalRecord: GoalRecord?
+        context.performAndWait {
+            let request: NSFetchRequest<GoalRecord> = GoalRecord.fetchRequest()
+            var predicates: [NSPredicate] = []
+            
+            if let predicate = request.predicate {
+                predicates.append(predicate)
+            }
+            
+            let calendar = Calendar.current
+            let startDate = calendar.startOfDay(for: date)
+            let endDate = calendar.date(byAdding: .day, value: 1, to: startDate)
+            
+            predicates.append(
+                NSPredicate(
+                    format: "goal.id = %@ AND date >= %@ AND date < %@",
+                    argumentArray: [
+                        goalID,
+                        startDate,
+                        endDate
+                    ]
+                )
+            )
+            request.predicate = NSCompoundPredicate(
+                andPredicateWithSubpredicates: predicates
+            )
+            request.fetchLimit = 1
+            
+            do {
+                goalRecord = try context.fetch(request).first
+            } catch {
+                fatalError("error: \(error)")
+            }
+        }
+        return goalRecord
+    }
+    
+    func deleteGoalRecord(for goalID: UUID, date: Date) {
+        context.performAndWait {
+            guard let goalRecord = readGoalRecord(for: goalID, date: date) else { return }
+
+            do {
+                context.delete(goalRecord)
+                try context.save()
+            } catch {
+                fatalError("error: \(error)")
+            }
+        }
+    }
+    
+}
